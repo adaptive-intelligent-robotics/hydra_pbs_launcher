b16d92fdb8a7d565baf6769344dcb95c6f2cc1e9
Antoine Cully <a.cully@imperial.ac.uk>
2023-05-09 14:11:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     ],
     install_requires=[
         "hydra-core>=1.1.0.dev7",
-        'submitit_pbs @ git+ssh://git@github.com/adaptive-intelligent-robotics/submitit_pbs@main#egg=submitit_pbs',
+        'submitit_pbs @ git+https://github.com/adaptive-intelligent-robotics/submitit_pbs@main#egg=submitit_pbs',
     ],
     include_package_data=True,
 )
